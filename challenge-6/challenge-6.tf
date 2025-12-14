b7739e43165d5d140b95714b9ca61ebf133771d8
anderson.mordhorst <anderson.mordhorst@gmail.com>
2025-12-14 10:56:16 -0300

    feat: resposta do desafio 6

--- challenge-6/challenge-6.tf
+++ challenge-6/challenge-6.tf
@@ -1,22 +1,47 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.80.0"
     }
   }
 }
 
 provider "aws" {
- region = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias = "readonly"
+  # Acces key and secret key will be picked up from de file created in base-folder/main.tf (default-creds.txt)
+  assume_role {
+    role_arn     = "arn:aws:iam::350644740974:role/ReadOnlyRole"
+    session_name = "terraform-session"
+  }
+}
+
+provider "aws" {
+  alias                    = "ec2"
+  profile                  = "ec2-access"
+  shared_config_files      = ["./.aws/config"]
+  shared_credentials_files = ["./.aws/credentials"]
+}
+
+provider "aws" {
+  alias                    = "iam"
+  profile                  = "iam-access"
+  shared_config_files      = ["./.aws/config"]
+  shared_credentials_files = ["./.aws/credentials"]
 }
 
 resource "aws_security_group" "allow_tls" {
-  name        = "demo-firewall"
-
+  provider = aws.ec2
+  name     = "demo-firewall"
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+  provider = aws.readonly
+}
 
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
@@ -24,8 +49,8 @@ output "account_id" {
 
 
 resource "aws_iam_role" "cw_full_access" {
-  name = "CloudWatchFullAccess"
-  managed_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchFullAccess"] 
+  provider = aws.iam
+  name     = "CloudWatchFullAccess"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -37,4 +62,10 @@ resource "aws_iam_role" "cw_full_access" {
       Action = "sts:AssumeRole"
     }]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  provider   = aws.iam
+  role       = aws_iam_role.cw_full_access.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
